b810afef7035567553e523c641e0db422fab0977
ProgrammerBS <12028223@student.hhs.nl>
2015-06-03 19:32:18 +0200

    Bazook en Helper Class added

---
+++ Doolhof/src/doolhof/Bazooka.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package doolhof;
+
+/**
+ *
+ * @author Singh
+ */
+public class Bazooka extends Item {
+    
+    public Bazooka(Tile myTile){
+        super(myTile);
+    }
+    
+}
---
+++ Doolhof/src/doolhof/Helper.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package doolhof;
+
+
+/**
+ *
+ * @author Singh
+ */
+public class Helper extends Item {
+    
+    public Helper(Tile myTile){
+        super(myTile);
+    }
+}
